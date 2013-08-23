d3a8cb3ac4bf68120261fc55a3baa44a998cb268
EpiphanyMachine <EpiphanyMachine@gmail.com>
2013-08-23 12:31:51 -0700

    raised test timeouts, opened issue #2 to address this

--- test/action_politician.js
+++ test/action_politician.js
@@ -4,7 +4,7 @@ describe('Action: politician', function(){
   var should = require('should');
 
   before(function(done){
-    this.timeout(5000);
+    this.timeout(20000);
     specHelper.prepare(0, function(api){
       apiObj = specHelper.cleanAPIObject(api);
       done();
@@ -12,7 +12,9 @@ describe('Action: politician', function(){
   });
 
   it('politician: no params', function(done){
+    this.timeout(20000);
     specHelper.apiTest.get("/politician", 0, {}, function(response, json){
+      console.log('length', json.results.length);
       json.results.should.have.lengthOf(100);
       json.results[0].should.have.property('bioguide_id');
       done();
@@ -20,6 +22,7 @@ describe('Action: politician', function(){
   });
 
   it('politician: valid id param', function(done){
+    this.timeout(20000);
     specHelper.apiTest.get("/politician", 0, {id:5}, function(response, json){
       json.results.should.have.lengthOf(1);
       json.results[0].should.have.property('bioguide_id');
@@ -28,6 +31,7 @@ describe('Action: politician', function(){
   });
 
   it('politician: invalid id param', function(done){
+    this.timeout(20000);
     specHelper.apiTest.get("/politician", 0, {id:'xs'}, function(response, json){
       json.results.should.have.lengthOf(100);
       json.results[83].should.have.property('bioguide_id');
@@ -36,8 +40,9 @@ describe('Action: politician', function(){
   });
 
   it('politician: all=true param', function(done){
+    this.timeout(20000);
     specHelper.apiTest.get("/politician", 0, {all:true}, function(response, json){
-      json.results.should.have.lengthOf(832);
+      json.results.should.have.lengthOf(831);
       json.results[150].should.have.property('bioguide_id');
       done();
     });
